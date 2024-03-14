3489676330e74293c354e6b236b63cd61f306090
sergiocutrim <97315931+sergiocutrim@users.noreply.github.com>
2024-03-14 10:59:13 -0400

    Add files via upload

---
+++ includes/footer-nav.php
@@ -0,0 +1,7 @@
+			<!-- Fine print navigation -->
+			<nav>
+				<a href="../Lab09/about/privacy-policy.php" title="Read our Privacy Policy">Privacy Policy</a> | 
+				<a href="../Lab09/about/privacy-policy.php#personal-information" title="Learn about how we treat your personal information">Use of Personal Information</a> | 
+				<a href="../Lab09/about/terms-of-use.php" title="Understand our Terms of Use">Terms of Use</a> | 
+				<a href="../Lab09/about/terms-of-use.php#limitation-liability" title="Our Limitation of Liability explained">Limitation of Liability</a>
+			</nav>
